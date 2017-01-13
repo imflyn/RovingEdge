7393d5e8d358f669e20c4a0c31fd7af8e7f5cb38
imflyn <flyn.yu@fandine.com>
2017-01-13 11:47:42 +0800

    把路径加入全局环境中

--- crawler/core/main.py
+++ crawler/core/main.py
@@ -1,4 +1,8 @@
 import sys
+from os.path import dirname
+
+sys.path.append(dirname(dirname(dirname(__file__))))
+
 import threading
 import time
 from concurrent.futures import ThreadPoolExecutor
@@ -12,53 +16,53 @@ from crawler.service.bus_station_service import BusStationService
 
 
 def crawl_bus_route():
-    bus_route_service = BusRouteService()
-    bus_route_list = bus_route_service.crawl_bus_route_data()
-    return bus_route_list
+	bus_route_service = BusRouteService()
+	bus_route_list = bus_route_service.crawl_bus_route_data()
+	return bus_route_list
 
 
 def crawl_bus_station(bus_station_list):
-    pool = ThreadPoolExecutor(4)
-    futures = []
+	pool = ThreadPoolExecutor(4)
+	futures = []
 
-    def callbacks_finished():
-        callbacks_finished.count += 1
-        log.info("爬取进度-->({index}/{count})".format(index=callbacks_finished.count, count=len(bus_station_list)))
-        if callbacks_finished.count == len(bus_station_list):
-            sys.exit()
+	def callbacks_finished():
+		callbacks_finished.count += 1
+		log.info("爬取进度-->({index}/{count})".format(index=callbacks_finished.count, count=len(bus_station_list)))
+		if callbacks_finished.count == len(bus_station_list):
+			sys.exit()
 
-    callbacks_finished.count = 0
+	callbacks_finished.count = 0
 
-    bus_station_service = BusStationService()
+	bus_station_service = BusStationService()
 
-    def crawl(bus_station):
-        bus_station_service.crawl_bus_station_data(bus_station)
-        callbacks_finished()
+	def crawl(bus_station):
+		bus_station_service.crawl_bus_station_data(bus_station)
+		callbacks_finished()
 
-    for bus_station in bus_station_list:
-        futures.append(pool.submit(crawl, bus_station))
+	for bus_station in bus_station_list:
+		futures.append(pool.submit(crawl, bus_station))
 
 
 if __name__ == '__main__':
-    def task():
-        from crawler.proxy.proxy_pool import proxy_pool
-        if config.USE_PROXY:
-            proxy_pool.start()
-        else:
-            proxy_pool.drop_proxy()
+	def task():
+		from crawler.proxy.proxy_pool import proxy_pool
+		if config.USE_PROXY:
+			proxy_pool.start()
+		else:
+			proxy_pool.drop_proxy()
 
 
-    def thread_task():
-        schedule = Scheduler()
-        schedule.every(30).minutes.do(task)
+	def thread_task():
+		schedule = Scheduler()
+		schedule.every(30).minutes.do(task)
 
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+		while True:
+			schedule.run_pending()
+			time.sleep(1)
 
 
-    thread = threading.Thread(target=thread_task)
-    thread.start()
-    task()
-    bus_station_list = crawl_bus_route()
-    crawl_bus_station(bus_station_list)
+	thread = threading.Thread(target=thread_task)
+	thread.start()
+	task()
+	bus_station_list = crawl_bus_route()
+	crawl_bus_station(bus_station_list)
---
+++ crawler/core/run.bat
@@ -0,0 +1,2 @@
+py -3 D:/Development/workspace_py/RovingEdge/crawler/core/main.py
+pause
