15a89ee3288c2d871da5368b60f27db7c49bf2eb
imflyn <imflyn@163.com>
2017-02-24 21:12:24 +0800

    优化爬虫

--- crawler/service/bus_route_service.py
+++ crawler/service/bus_route_service.py
@@ -8,82 +8,94 @@ from crawler.model.bus_route import BusRoute
 
 
 class BusRouteService(object):
-	TABLE_BUS_ROUTE = 'bus_route'
-	TABLE_BUS_STATION_ALL = 'bus_station_all'
+    TABLE_BUS_ROUTE = 'bus_route'
+    TABLE_BUS_STATION_ALL = 'bus_station_all'
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def request_bus_route_data(self):
-		response = utils.http_get(constants.URL_BUS_ROUTE, timeout=60)
-		return response.text
+    def request_bus_route_data(self):
+        response = utils.http_get(constants.URL_BUS_ROUTE, timeout=60)
+        return response.text
 
-	def handle_bus_route_data(self, xml_content):
-		xml = etree.XML(xml_content)
-		content_list = xml.xpath("//LPLine")
-		bus_route_list = []
-		for content_item in content_list:
-			bus_route = BusRoute()
-			bus_route.id = utils.get_uuid()
-			bus_route.GUID = content_item.xpath('LPGUID')[0].text
-			bus_route.route_name = content_item.xpath('LPLineName')[0].text
-			bus_route.start_station = content_item.xpath('LPFStdName')[0].text
-			bus_route.end_station = content_item.xpath('LPEStdName')[0].text
-			bus_route.start_time = content_item.xpath('LPFStdFTime')[0].text
-			bus_route.end_time = content_item.xpath('LPFStdETime')[0].text
-			bus_route.peak_departure_interval = content_item.xpath('LPIntervalH')[0].text
-			bus_route.low_departure_interval = content_item.xpath('LPIntervalN')[0].text
-			bus_route.direction = content_item.xpath('LPDirection')[0].text
-			pass_way = content_item.xpath('LPLineDirect')[0].text
-			pass_way = utils.replace_number_symbol(pass_way)
-			if pass_way is not None and not len(pass_way) == 0:
-				pass_way = pass_way.replace('\n', '')
-				bus_route.pass_way = pass_way.split('->')
-			stations = content_item.xpath('LPStandName')[0].text
-			stations = utils.replace_number_symbol(stations)
-			if stations is not None and not len(stations) == 0:
-				stations = stations.replace('\n', '')
-				bus_route.stations = stations.split('、')
-				bus_route.station_number = len(bus_route.stations)
-			bus_route_list.append(bus_route)
-		return bus_route_list
+    def handle_bus_route_data(self, xml_content):
+        xml = etree.XML(xml_content)
+        content_list = xml.xpath("//LPLine")
+        bus_route_list = []
+        for content_item in content_list:
+            bus_route = BusRoute()
+            bus_route.id = utils.get_uuid()
+            bus_route.GUID = content_item.xpath('LPGUID')[0].text.lower()
+            bus_route.route_name = content_item.xpath('LPLineName')[0].text
+            bus_route.start_station = content_item.xpath('LPFStdName')[0].text
+            bus_route.end_station = content_item.xpath('LPEStdName')[0].text
+            bus_route.start_time = content_item.xpath('LPFStdFTime')[0].text
+            bus_route.end_time = content_item.xpath('LPFStdETime')[0].text
+            bus_route.peak_departure_interval = content_item.xpath('LPIntervalH')[0].text
+            bus_route.low_departure_interval = content_item.xpath('LPIntervalN')[0].text
+            bus_route.direction = content_item.xpath('LPDirection')[0].text
+            pass_way = content_item.xpath('LPLineDirect')[0].text
+            pass_way = utils.replace_number_symbol(pass_way)
+            if pass_way is not None and not len(pass_way) == 0:
+                pass_way = pass_way.replace('\n', '')
+                bus_route.pass_way = pass_way.split('->')
+            stations = content_item.xpath('LPStandName')[0].text
+            stations = utils.replace_number_symbol(stations)
+            if stations is not None and not len(stations) == 0:
+                stations = stations.replace('\n', '')
+                bus_route.stations = stations.split('、')
+                bus_route.station_number = len(bus_route.stations)
+            bus_route_list.append(bus_route)
+        return bus_route_list
 
-	def save_bus_route_data_to_db(self, bus_route_list):
-		collection = mongodb_service.get_collection(config.mongodb, self.TABLE_BUS_ROUTE)
-		data = []
-		for bus_route in bus_route_list:
-			data.append(bus_route.__dict__)
-		# 删除整个表的数据再批量插入
-		mongodb_service.delete_all_data(collection)
-		log.info('爬取公交线路-->删除数据库中原有数据成功')
-		mongodb_service.insert_all(collection, data)
-		log.info('爬取公交线路-->数据插入数据库成功')
+    def save_bus_route_data_to_db(self, bus_route_list):
+        collection = mongodb_service.get_collection(config.mongodb, self.TABLE_BUS_ROUTE)
+        data = []
+        for bus_route in bus_route_list:
+            data.append(bus_route.__dict__)
+        # 删除整个表的数据再批量插入
+        mongodb_service.delete_all_data(collection)
+        log.info('爬取公交线路-->删除数据库中原有数据成功')
+        mongodb_service.insert_all(collection, data)
+        log.info('爬取公交线路-->数据插入数据库成功')
+        self.handler_error_data(collection)
+        log.info('爬取公交线路-->修改部分错误数据成功')
 
-	def crawl_bus_route_data(self) -> []:
-		log.info('爬取公交线路开始')
-		try:
-			bus_route_xml = self.request_bus_route_data()
-			log.info('爬取公交线路XML成功')
-			bus_route_list = self.handle_bus_route_data(bus_route_xml)
-			log.info('处理公交线路数据成功')
-			self.save_bus_route_data_to_db(bus_route_list)
-			log.info('爬取公交线路成功')
-			bus_station_list = self.save_station_list_to_db(bus_route_list)
-			return bus_station_list
-		except Exception as e:
-			log.info('爬取公交线路失败')
-			log.error(e)
+    def crawl_bus_route_data(self) -> []:
+        log.info('爬取公交线路开始')
+        try:
+            bus_route_xml = self.request_bus_route_data()
+            log.info('爬取公交线路XML成功')
+            bus_route_list = self.handle_bus_route_data(bus_route_xml)
+            log.info('处理公交线路数据成功')
+            self.save_bus_route_data_to_db(bus_route_list)
+            log.info('爬取公交线路成功')
+            bus_station_list = self.save_station_list_to_db(bus_route_list)
+            return bus_station_list
+        except Exception as e:
+            log.info('爬取公交线路失败')
+            log.error(e)
 
-	def save_station_list_to_db(self, bus_route_list):
-		log.info('爬取公交线路-->去除重复公交站台开始')
-		bus_station_list = []
-		for bus_route in bus_route_list:
-			bus_station_list.extend(bus_route.stations)
-		bus_station_list = list(set(bus_station_list))
-		log.info('爬取公交线路-->去除重复公交站台成功')
-		collection = mongodb_service.get_collection(config.mongodb, self.TABLE_BUS_STATION_ALL)
-		mongodb_service.delete_all_data(collection)
-		log.info('爬取公交线路-->删除旧公交站台集合数据成功')
-		mongodb_service.insert(collection, {'bus_station_list': bus_station_list})
-		log.info('爬取公交线路-->保存公交站台集合数据成功')
-		return bus_station_list
+    def save_station_list_to_db(self, bus_route_list):
+        log.info('爬取公交线路-->去除重复公交站台开始')
+        bus_station_list = []
+        for bus_route in bus_route_list:
+            bus_station_list.extend(bus_route.stations)
+        bus_station_list = list(set(bus_station_list))
+        log.info('爬取公交线路-->去除重复公交站台成功')
+        collection = mongodb_service.get_collection(config.mongodb, self.TABLE_BUS_STATION_ALL)
+        mongodb_service.delete_all_data(collection)
+        log.info('爬取公交线路-->删除旧公交站台集合数据成功')
+        mongodb_service.insert(collection, {'bus_station_list': bus_station_list})
+        log.info('爬取公交线路-->保存公交站台集合数据成功')
+        return bus_station_list
+
+    def handler_error_data(self, collection):
+        collection.update_one({'GUID': 'aaaaaaaa-aaaa-aaaa-aaaa-000000000001'}, {"$set": {'GUID': '778d674e-b483-4a13-9aaf-d1acb705d10d'}})
+        collection.update_one({'GUID': 'aaaaaaaa-aaaa-aaaa-aaaa-000000000002'}, {"$set": {'GUID': '4f63f3a0-c003-43b1-b9a1-88240efb0800'}})
+        collection.update_one({'GUID': 'aaaaaaaa-aaaa-aaaa-aaaa-000000000003'}, {"$set": {'GUID': '96aa2ec5-031a-4e84-a358-611f08f07947'}})
+        collection.update_one({'GUID': 'aaaaaaaa-aaaa-aaaa-aaaa-000000000004'}, {"$set": {'GUID': '374cb92a-5886-432b-a1fc-1f997ff43662'}})
+        collection.update_one({'GUID': 'aaaaaaaa-aaaa-aaaa-aaaa-000000000005'}, {"$set": {'GUID': ''}})
+        collection.update_one({'GUID': 'aaaaaaaa-aaaa-aaaa-aaaa-000000000006'}, {"$set": {'GUID': 'e7d3789b-fcef-42de-ba49-d0e9a42f452a'}})
+        collection.update_one({'GUID': 'aaaaaaaa-aaaa-aaaa-aaaa-000000000007'}, {"$set": {'GUID': '1b414ade-6468-4242-ad36-c8ac3e13a2b7'}})
+        collection.update_one({'GUID': 'aaaaaaaa-aaaa-aaaa-aaaa-000000000008'}, {"$set": {'GUID': '084ef4a4-e549-46a0-8aff-5c77092745ae'}})
--- crawler/service/bus_station_service.py
+++ crawler/service/bus_station_service.py
@@ -12,163 +12,163 @@ from crawler.proxy.proxy_pool import proxy_pool
 
 
 class BusStationService(object):
-	TABLE_NAME = 'bus_station'
+    TABLE_NAME = 'bus_station'
 
-	def __init__(self):
-		self.collection = mongodb_service.get_collection(config.mongodb, self.TABLE_NAME)
+    def __init__(self):
+        self.collection = mongodb_service.get_collection(config.mongodb, self.TABLE_NAME)
 
-	def handle_bus_station_list_data(self, content, station_name) -> []:
-		log.info('爬取公交站台-->处理<{station_name}>站台信息开始'.format(station_name=station_name))
-		try:
-			html = etree.HTML(content)
-		except XMLSyntaxError:
-			log.info('爬取公交站台-->处理<{station_name}>站台信息失败'.format(station_name=station_name))
-			return []
-		except ValueError:
-			log.info('爬取公交站台-->处理<{station_name}>站台信息失败'.format(station_name=station_name))
-			return []
-		try:
-			content_list = html.xpath("//span[@id='MainContent_DATA']//table//tr")
-		except XPathEvalError:
-			log.info('爬取公交站台-->处理<{station_name}>站台信息失败'.format(station_name=station_name))
-			return []
+    def handle_bus_station_list_data(self, content, station_name) -> []:
+        log.info('爬取公交站台-->处理<{station_name}>站台信息开始'.format(station_name=station_name))
+        try:
+            html = etree.HTML(content)
+        except XMLSyntaxError:
+            log.info('爬取公交站台-->处理<{station_name}>站台信息失败'.format(station_name=station_name))
+            return []
+        except ValueError:
+            log.info('爬取公交站台-->处理<{station_name}>站台信息失败'.format(station_name=station_name))
+            return []
+        try:
+            content_list = html.xpath("//span[@id='MainContent_DATA']//table//tr")
+        except XPathEvalError:
+            log.info('爬取公交站台-->处理<{station_name}>站台信息失败'.format(station_name=station_name))
+            return []
 
-		station_list = []
-		for index, content_item in enumerate(content_list):
-			if index == 0:
-				continue
+        station_list = []
+        for index, content_item in enumerate(content_list):
+            if index == 0:
+                continue
 
-			name = content_item.getchildren()[0].xpath('a')[0].text
-			number = content_item.getchildren()[1].text
-			area = content_item.getchildren()[2].text
-			road = content_item.getchildren()[3].text
-			road_segment = content_item.getchildren()[4].text
-			road_direction = content_item.getchildren()[5].text
+            name = content_item.getchildren()[0].xpath('a')[0].text
+            number = content_item.getchildren()[1].text
+            area = content_item.getchildren()[2].text
+            road = content_item.getchildren()[3].text
+            road_segment = content_item.getchildren()[4].text
+            road_direction = content_item.getchildren()[5].text
 
-			name = utils.replace_number_symbol(name)
+            name = utils.replace_number_symbol(name)
 
-			bus_station = BusStation()
-			bus_station.id = utils.get_uuid()
-			bus_station.name = name
-			bus_station.number = number
-			bus_station.area = area
-			bus_station.road = road
-			bus_station.road_segment = road_segment
-			bus_station.road_direction = road_direction
-			station_list.append(bus_station)
-		log.info('爬取公交站台-->处理<{station_name}>站台信息成功，获得<{station_count}>个站台信息'.format(station_name=station_name, station_count=len(station_list)))
-		return station_list
+            bus_station = BusStation()
+            bus_station.id = utils.get_uuid()
+            bus_station.name = name
+            bus_station.number = number
+            bus_station.area = area
+            bus_station.road = road
+            bus_station.road_segment = road_segment
+            bus_station.road_direction = road_direction
+            station_list.append(bus_station)
+        log.info('爬取公交站台-->处理<{station_name}>站台信息成功，获得<{station_count}>个站台信息'.format(station_name=station_name, station_count=len(station_list)))
+        return station_list
 
-	def request_bus_station_data(self, station_name):
-		url = 'http://www.szjt.gov.cn/apts/default.aspx'
-		data = {
-			'ctl00$MainContent$StandName': station_name,
-			'ctl00$MainContent$SearchCode': '搜索',
-			'__VIEWSTATE': '/wEPDwULLTE5ODM5MjcxNzlkZG5FgcZjEw/Xcik6rLaQKQqjiJG1N/LcEaJpbqk1zMfT',
-			'__VIEWSTATEGENERATOR': '7BCA6D38',
-			'__EVENTVALIDATION': '/wEWBQKV37ujDALq+uyKCAKkmJj/DwL0+sTIDgLl5vKEDjV831PkO9I9rzINcIDwIwK31J8x9g8zuNZKL7+XkZX5'
-		}
-		headers = {
-			'Referer': 'http://www.szjt.gov.cn/apts/default.aspx',
-			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-		}
-		if config.USE_PROXY:
-			retry_time = 1
-			while True:
-				ip = proxy_pool.random_choice_proxy()
-				proxies = {
-					"http": ip
-				}
-				log.info('爬取公交站台-->处理<{station_name}>站台，第<{retry_time}>次尝试,使用代理<{ip}>发送Http请求'.format(station_name=station_name, ip=ip,
-				                                                                                      retry_time=retry_time))
-				try:
-					response = requests.post(url, data=data, headers=headers, timeout=30, proxies=proxies)
-					response.encoding = 'utf-8'
-					if '仿真电子' not in response.text:
-						raise Exception()
+    def request_bus_station_data(self, station_name):
+        url = 'http://www.szjt.gov.cn/apts/default.aspx'
+        data = {
+            'ctl00$MainContent$StandName': station_name,
+            'ctl00$MainContent$SearchCode': '搜索',
+            '__VIEWSTATE': '/wEPDwULLTE5ODM5MjcxNzlkZG5FgcZjEw/Xcik6rLaQKQqjiJG1N/LcEaJpbqk1zMfT',
+            '__VIEWSTATEGENERATOR': '7BCA6D38',
+            '__EVENTVALIDATION': '/wEWBQKV37ujDALq+uyKCAKkmJj/DwL0+sTIDgLl5vKEDjV831PkO9I9rzINcIDwIwK31J8x9g8zuNZKL7+XkZX5'
+        }
+        headers = {
+            'Referer': 'http://www.szjt.gov.cn/apts/default.aspx',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        }
+        if config.USE_PROXY:
+            retry_time = 1
+            while True:
+                ip = proxy_pool.random_choice_proxy()
+                proxies = {
+                    "http": ip
+                }
+                log.info('爬取公交站台-->处理<{station_name}>站台，第<{retry_time}>次尝试,使用代理<{ip}>发送Http请求'.format(station_name=station_name, ip=ip,
+                                                                                                      retry_time=retry_time))
+                try:
+                    response = requests.post(url, data=data, headers=headers, timeout=30, proxies=proxies)
+                    response.encoding = 'utf-8'
+                    if '仿真电子' not in response.text:
+                        raise Exception()
 
-					proxy_pool.add_success_time(ip)
-					break
-				except:
-					log.info('爬取公交站台-->处理<{station_name}>站台，第<{retry_time}>次尝试,使用代理<{ip}>发送Http请求失败'
-					         .format(station_name=station_name, ip=ip, retry_time=retry_time))
-					retry_time += 1
-					proxy_pool.add_failed_time(ip)
-		else:
-			try:
-				response = requests.post(url, data=data, headers=headers, timeout=20)
-			except Exception as e:
-				log.error(e)
-				return None
-		if response is None:
-			return None
-		return response.text
+                    proxy_pool.add_success_time(ip)
+                    break
+                except:
+                    log.info('爬取公交站台-->处理<{station_name}>站台，第<{retry_time}>次尝试,使用代理<{ip}>发送Http请求失败'
+                             .format(station_name=station_name, ip=ip, retry_time=retry_time))
+                    retry_time += 1
+                    proxy_pool.add_failed_time(ip)
+        else:
+            try:
+                response = requests.post(url, data=data, headers=headers, timeout=20)
+            except Exception as e:
+                log.error(e)
+                return None
+        if response is None:
+            return None
+        return response.text
 
-	def request_bus_station_data2(self, station_name):
-		url = 'http://www.szjt.gov.cn/BusQuery/default.aspx?cid=175ecd8d-c39d-4116-83ff-109b946d7cb4'
-		data = {
-			'ctl00$MainContent$StandName': station_name,
-			'ctl00$MainContent$SearchCode': '搜索',
-			'__VIEWSTATE': '/wEPDwULLTE5ODM5MjcxNzlkZAmiJJ2tnI/uORAHha02uiTP20zkcIPFI/03+QRVt2jm',
-			'__VIEWSTATEGENERATOR': '1DDAEB65',
-			'__EVENTVALIDATION': '/wEWBQLCvY65BALq+uyKCAKkmJj/DwL0+sTIDgLl5vKEDsL/hpJTaG5nWiRLbcu6Hd+JXovreFUihHtwVJ7zNsPc'
-		}
-		headers = {
-			'Referer': 'http://www.szjt.gov.cn/BusQuery/default.aspx?cid=175ecd8d-c39d-4116-83ff-109b946d7cb4',
-			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-		}
-		if config.USE_PROXY:
-			retry_time = 1
-			while True:
-				ip = proxy_pool.random_choice_proxy()
-				proxies = {
-					"http": ip
-				}
-				log.info('爬取公交站台-->处理<{station_name}>站台，第<{retry_time}>次尝试,使用代理<{ip}>发送Http请求'.format(station_name=station_name, ip=ip,
-				                                                                                      retry_time=retry_time))
-				try:
-					response = requests.post(url, data=data, headers=headers, timeout=30, proxies=proxies)
-					response.encoding = 'utf-8'
-					if '仿真电子' not in response.text:
-						raise Exception()
+    def request_bus_station_data2(self, station_name):
+        url = 'http://www.szjt.gov.cn/BusQuery/default.aspx?cid=175ecd8d-c39d-4116-83ff-109b946d7cb4'
+        data = {
+            'ctl00$MainContent$StandName': station_name,
+            'ctl00$MainContent$SearchCode': '搜索',
+            '__VIEWSTATE': '/wEPDwULLTE5ODM5MjcxNzlkZAmiJJ2tnI/uORAHha02uiTP20zkcIPFI/03+QRVt2jm',
+            '__VIEWSTATEGENERATOR': '1DDAEB65',
+            '__EVENTVALIDATION': '/wEWBQLCvY65BALq+uyKCAKkmJj/DwL0+sTIDgLl5vKEDsL/hpJTaG5nWiRLbcu6Hd+JXovreFUihHtwVJ7zNsPc'
+        }
+        headers = {
+            'Referer': 'http://www.szjt.gov.cn/BusQuery/default.aspx?cid=175ecd8d-c39d-4116-83ff-109b946d7cb4',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        }
+        if config.USE_PROXY:
+            retry_time = 1
+            while True:
+                ip = proxy_pool.random_choice_proxy()
+                proxies = {
+                    "http": ip
+                }
+                log.info('爬取公交站台-->处理<{station_name}>站台，第<{retry_time}>次尝试,使用代理<{ip}>发送Http请求'.format(station_name=station_name, ip=ip,
+                                                                                                      retry_time=retry_time))
+                try:
+                    response = requests.post(url, data=data, headers=headers, timeout=30, proxies=proxies)
+                    response.encoding = 'utf-8'
+                    if '仿真电子' not in response.text:
+                        raise Exception()
 
-					proxy_pool.add_success_time(ip)
-					break
-				except:
-					log.info('爬取公交站台-->处理<{station_name}>站台，第<{retry_time}>次尝试,使用代理<{ip}>发送Http请求失败'
-					         .format(station_name=station_name, ip=ip, retry_time=retry_time))
-					retry_time += 1
-					proxy_pool.add_failed_time(ip)
-		else:
-			try:
-				response = requests.post(url, data=data, headers=headers, timeout=20)
-			except Exception as e:
-				log.error(e)
-				return None
-		if response is None:
-			return None
-		return response.text
+                    proxy_pool.add_success_time(ip)
+                    break
+                except:
+                    log.info('爬取公交站台-->处理<{station_name}>站台，第<{retry_time}>次尝试,使用代理<{ip}>发送Http请求失败'
+                             .format(station_name=station_name, ip=ip, retry_time=retry_time))
+                    retry_time += 1
+                    proxy_pool.add_failed_time(ip)
+        else:
+            try:
+                response = requests.post(url, data=data, headers=headers, timeout=20)
+            except Exception as e:
+                log.error(e)
+                return None
+        if response is None:
+            return None
+        return response.text
 
-	def save_bus_station_data_to_db(self, bus_station: BusStation):
-		try:
-			if not self.collection.find_one({'number': bus_station.number}):
-				self.collection.insert_one(bus_station.__dict__)
-			else:
-				self.collection.replace_one({'number': bus_station.number}, bus_station.__dict__)
-		except Exception as e:
-			log.error(e)
+    def save_bus_station_data_to_db(self, bus_station: BusStation):
+        try:
+            if not self.collection.find_one({'number': bus_station.number}):
+                self.collection.insert_one(bus_station.__dict__)
+            else:
+                self.collection.replace_one({'number': bus_station.number}, bus_station.__dict__)
+        except Exception as e:
+            log.error(e)
 
-	def crawl_bus_station_data(self, station_name):
-		bus_station_html = self.request_bus_station_data(station_name)
-		bus_station_list = self.handle_bus_station_list_data(bus_station_html, station_name)
-		if isinstance(bus_station_list, collections.Iterable) and len(bus_station_list) > 0:
-			log.info('爬取公交站台-->保存<{station_name}>站台信息开始'.format(station_name=station_name))
-			for bus_station in bus_station_list:
-				self.save_bus_station_data_to_db(bus_station)
-			log.info('爬取公交站台-->保存<{station_name}>站台信息成功'.format(station_name=station_name))
-		else:
-			log.info('爬取公交站台-->站台名<{station_name}>没有获取到站台信息'.format(station_name=station_name))
+    def crawl_bus_station_data(self, station_name):
+        bus_station_html = self.request_bus_station_data(station_name)
+        bus_station_list = self.handle_bus_station_list_data(bus_station_html, station_name)
+        if isinstance(bus_station_list, collections.Iterable) and len(bus_station_list) > 0:
+            log.info('爬取公交站台-->保存<{station_name}>站台信息开始'.format(station_name=station_name))
+            for bus_station in bus_station_list:
+                self.save_bus_station_data_to_db(bus_station)
+            log.info('爬取公交站台-->保存<{station_name}>站台信息成功'.format(station_name=station_name))
+        else:
+            log.info('爬取公交站台-->站台名<{station_name}>没有获取到站台信息'.format(station_name=station_name))
